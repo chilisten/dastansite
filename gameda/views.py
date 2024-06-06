23a4364d13a280d30750ac69055c904514323698
chilisten <dastankenzebekov099@gmail.com>
2024-06-06 14:59:49 +0600

    first commit

---
+++ gameda/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
+from .models import Post
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 4
+    template_name = 'blog/post/list.html'
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post,
+                             status=Post.Status.PUBLISHED,
+                             slug=post,
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
+    return render(request, 'blog/post/detail.html', {'post': post})
